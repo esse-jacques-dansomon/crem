f66acb090efc875d244744d82a75a6596bb71a24
Fulbert gato junior Adanhodou <gatojunior.co@gmail.com>
2024-12-08 23:26:50 +0000

    first commit
    Signed-off-by: Fulbert gato junior Adanhodou <gatojunior.co@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customYellow: 'rgb(249, 184, 7)', 
+      },
+    },
+  },
+  plugins: [],
+}
+
